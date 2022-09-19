751f4ece770da29a38ca32e1e2248a9b9e3e9dc7
Alexy Dubois <alexy.dubois62@gmail.com>
2022-09-19 16:05:19 +0200

    model user add role

--- EpicEvents/auth_epic_events/models.py
+++ EpicEvents/auth_epic_events/models.py
@@ -1,6 +1,13 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-
 class User(AbstractUser):
-    pass
+    SUPPORT = 1
+    SELLER = 2
+
+    ROLE_CHOICES = (
+        (SUPPORT, 'Support'),
+        (SELLER, 'Seller'),
+    )
+
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
--- EpicEvents/auth_epic_events/views.py
+++ EpicEvents/auth_epic_events/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from auth_epic_events.serializers import UsersSerializer
+from auth_epic_events.models import User
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+class SignUpAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            obj = UsersSerializer(data=request.data)
+            if obj.is_valid():
+                User.objects.create_user(username = obj['username'].value,
+                                         password = obj['password'].value,role =obj['role'].value)
+                return Response({'Message': 'valid sign up'}, status=status.HTTP_200_OK)
+            return Response(obj.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response({'Message': 'Something Failed due to {}'.format(str(e))}, status=status.HTTP_400_BAD_REQUEST)
